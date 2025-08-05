4ad70af3ede4e4f97dc8885c1dfad7655c0f1836
Aaditya03k <220701003@rajalakshmi.edu.in>
2025-08-05 21:26:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
  import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/classroom-booking-app/', // 👈 Important for GitHub Pages
+  
   plugins: [react()]
 })
